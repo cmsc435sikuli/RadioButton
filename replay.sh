c37f99c88605e8a35b6d6e473ca4ec5d6f326421
peter <iofthemourning@gmail.com>
2011-03-30 23:27:43 -0400

    added crappy replay script

---
+++ replay.sh
@@ -0,0 +1,2 @@
+./greplayer jfc -g GUITAR-Default.GUI -e efg.efg -t tests/t_e3442548224_e3442548224_e3442548224_e3442548224_e3442548224_e3442548224_e3442548224_e3442548224_e3442548224_e1009749812.tst --main-class Project --urls /home/peter/sguitar/RadioButton/Project.jar --config-file config/configuration.xml -m $1
+
