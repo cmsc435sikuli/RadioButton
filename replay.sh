b23430a48f714f29d8d6fba085b8879cc6b4113e
peter <iofthemourning@gmail.com>
2011-03-30 23:40:47 -0400

    test argument

--- replay.sh
+++ replay.sh
@@ -2,5 +2,5 @@ C_DIR=$(pwd)
 JAR_DIR=$(cd $(dirname $0) && pwd)
 cd $JAR_DIR
 cd ..
-./greplayer jfc -g GUITAR-Default.GUI -e efg.efg -t tests/t_e3442548224_e3442548224_e3442548224_e3442548224_e3442548224_e3442548224_e3442548224_e3442548224_e3442548224_e1009749812.tst --main-class Project --urls $JAR_DIR/Project.jar --config-file config/configuration.xml -m $1
+./greplayer jfc -g GUITAR-Default.GUI -e efg.efg -t $2 --main-class Project --urls $JAR_DIR/Project.jar --config-file config/configuration.xml -m $1
 cd $C_DIR
